29004fb462f5b271ad2f7bb2ef07ad53d4033e27
Dustdev8 <ruslanmammadzada8@gmail.com>
2024-10-18 16:47:18 +0300

    added the functionalities for separating the words by commas and dots when counting

--- app/src/main/java/com/example/second_app_ruslan/utilities/WordCounter.java
+++ app/src/main/java/com/example/second_app_ruslan/utilities/WordCounter.java
@@ -3,7 +3,7 @@ package com.example.second_app_ruslan.utilities;
 public class WordCounter {
     public static int GetWordsCount(String input) {
 
-        String[] words = input.trim().split("\\s+");
+        String[] words = input.trim().split("[\\s,.]+");
 
         return words.length;
     }
